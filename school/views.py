aa49212a9a2fbddfc2505c794814c0b0e20dafba
Evgen <evgen.k.95@yandex.ru>
2023-01-26 11:09:58 +0300

    Initial commit

---
+++ school/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'name'
+
+    object_list = Student.objects.prefetch_related('teachers').order_by(ordering).all()
+
+    context = {
+        'object_list': object_list,
+    }
+    
+    return render(request, template, context)
